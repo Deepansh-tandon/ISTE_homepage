3965532131d8fbd8fd7c3a7f948e924d9f8e0818
Bhavik3000 <bhavik00003@gmail.com>
2024-07-14 20:49:01 +0530

    cards made

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,10 +3,21 @@ import styles from "./Cards.module.css"
 
 export const Cards = () => {
   return (
-      <div className={styles.container} id = "boxesPage">
-      <div className={styles.events}>
-             </div>
+    <>
+    <div className={styles.container}>
+      <div className={styles.column1}>
+      <div className={styles.events}></div>
+      <div className={styles.bottomcards}>
+        <div className={styles.team}></div>
+        <div className={styles.alumni}></div>
       </div>
+      </div>
+      <div className={styles.column2}>
+      <div className={styles.about}></div>
+      <div className={styles.pastprojects}></div>
+      </div>
+     </div>
+    </>
   )
 }
 
